1b0f1e544ccfa7b05629c8bc190687520fa158b4
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-06-30 06:36:20 +1200

    add root and internal api path

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const express = require('express')
 
-const widgets = require('./routes/widgets') //change to ./routes/flavours
+const flavours = require('./routes/flavours') //root
 
 const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/widgets', widgets) //change to /api/v1/flavours, flavours
+server.use('/api/v1/flavours', flavours) //internal api
 
 module.exports = server
