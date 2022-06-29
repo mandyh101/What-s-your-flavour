0403579d1d9747abc5112d6d64d7ace23001514b
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-06-30 11:29:35 +1200

    add get all food function

--- server/db/db.js
+++ server/db/db.js
@@ -3,14 +3,16 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-  getWidgets,
+  getAllFood,
 }
 
-function getWidgets(db = connection) {
-  return db('widgets').select()
+function getAllFood(db = connection) {
+  return db('food').select()
 }
 
 //1. update file name
-//2. Write  getAllFlavours function
+//2. Write  getAllFood names or getAllFood function?
+//3. Wite a function that gets all flavours that match a food by id/food_id
+//4. write a function that gets a flavour combo by ids?
 //3. Write a function to get all flavours that match an id
 //4. Write a function to get all flavours that match an id and select a random one (this one might go inside the routes function?)
