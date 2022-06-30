6e2142005a98d0c03be761a04942e2ae399ac15c
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-01 07:17:37 +1200

    create handle onChange function to get foodSelected value

--- client/components/FoodSelector.jsx
+++ client/components/FoodSelector.jsx
@@ -3,22 +3,30 @@ import { getFoodData } from "../apiClient";
 
 export function FoodSelector(){
   const [foodNames, setFoodNames] = useState([])
+  // const [foodSelected, setFoodSelected] = useState([])
 
   useEffect(()=>{
     getFoodData()
     .then((foodData) => {
-    setFoodNames(foodData.map((food) => <option key={food.id}>{food.name}</option>)) 
+    setFoodNames(foodData.map((food) => <option key={food.id} value={food.name}>{food.name}</option>)) 
     })
     .catch((err)=> {
       console.log(err)
     })
   },[])
 
+  function handleOnChange(e){
+    const foodSelected = e.target.value
+    console.log(foodSelected)
+    // setFoodSelected(foodNames.filter((food) => food === e.target.value))
+    // console.log(foodSelected)
+  }
+
 
   return(
     <div>
       <label htmlFor="foodOptions">Select your food of choice:</label>
-      <select id="foodOptions" name="foodOptions">{foodNames}</select>
+      <select id="foodOptions" name="foodOptions" onChange={e => handleOnChange(e)}>{foodNames}</select>
     </div>
   )
 }
