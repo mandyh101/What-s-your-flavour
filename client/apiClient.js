bf74e503a91ab5db69cda9d572c2680e2fbc82c6
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-06-30 12:10:36 +1200

    write function to get foodData from server

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,6 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import request from 'superagent'
 // eslint-disable-next-line no-unused-vars
-const widgetUrl = '/api/v1/widgets/'
+const foodUrl = '/api/v1/food/'
 
-export function getWidgets() {}
+export function getFoodData() {
+  return request
+    .get(foodUrl)
+    .then((response) => {
+      const foodData = response.body
+      return foodData
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
