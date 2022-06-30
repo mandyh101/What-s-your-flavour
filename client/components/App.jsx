4db52340f1248986ebcc75af9764e07221a49757
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-06-30 20:47:30 +1200

    import FoodSelector

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { FoodSelector } from './FoodSelector'
 
 function App() {
   return (
     <div>
       <h1>What's your flavour?</h1>
+      <FoodSelector />
     </div>
   )
 }
