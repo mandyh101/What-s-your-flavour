555e69a4cb16b0fb12e5d9687c0242b21eeb8be6
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-02 11:15:40 +1200

    edit child components

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import FoodSelector from './FoodSelector';
 import Title from './Title'
+import Header from './Header'
 
 function Home () {
 
   return ( 
     <>
+    <Header />
     <Title />
     <FoodSelector />
     </>
