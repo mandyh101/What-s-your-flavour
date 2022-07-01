4444f8d9d5d93b0dc8b9bfe6b94674eab85e3de3
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-02 10:48:15 +1200

    add new components for home

---
+++ client/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import FoodSelector from './FoodSelector';
+import Title from './Title'
+
+function Home () {
+
+  return ( 
+    <>
+    <Title />
+    <FoodSelector />
+    </>
+    
+   );
+}
+
+export default Home ;
