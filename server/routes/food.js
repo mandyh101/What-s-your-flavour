c861a046b6a2947d20e85a4177ffa38ee2e01791
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-02 15:25:51 +1200

    write route to send a json of flavours depending on what food is selected

--- server/routes/food.js
+++ server/routes/food.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../db/db')
 
+//get all food data
 router.get('/', (req, res) => {
   db.getAllFood()
     .then((food) => {
@@ -13,8 +14,16 @@ router.get('/', (req, res) => {
     })
 })
 
-// router.get('/', (req,res) => {
-//   db.get
-// })
+//get flavour combos according to food id
+router.get('/:foodId', (req, res) => {
+  db.getAllFlavoursByFood(req.params.foodId)
+    .then((flavours) => {
+      res.json(flavours)
+    })
+    .catch((err) => {
+      res.status(500).send('Something went wrong!')
+      console.error(err)
+    })
+})
 
 module.exports = router
