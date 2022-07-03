89b2b0c9572e9254a7d222049e5bad242e8efce4
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-03 13:15:26 +1200

    render flavours by selected food

--- client/components/DisplayFlavours.jsx
+++ client/components/DisplayFlavours.jsx
@@ -1,45 +1,36 @@
 
 import React, {useState, useEffect} from "react";
 import { getFoodData, getAllFlavourCombos } from "../apiClient";
-import FoodSelector from "./FoodSelector";
 
-function DisplayFlavour(props) {
-  const foodselected = props.food
-  const [foodObj, setFoodObj] = useState({})
-  console.log('food', foodselected)
+function DisplayFlavour({food}) {
+  console.log({food})
+  // const foodselected = props.food
+  const [combos, setCombos] = useState([])
+  // console.log('food', foodselected)
   
   useEffect(()=>{
     getFoodData()
     .then((foodData) => {
-      setFoodObj(foodData.find((food) => food.name === foodselected))
-      console.log(foodObj) 
-    }).then ((foodObj) => {
-      displayFlavourCombo(foodObj)
+      const obj = foodData.find((item) => item.name === food)
+      return(obj) 
+    }).then ((obj) => {
+      const foodId = String(obj.id)
+      return getAllFlavourCombos(foodId)
+    }).then ((flavourCombos) => {
+      console.log('combos' , flavourCombos)
+      setCombos(flavourCombos)
+      console.log('flavs', combos)
     })
     .catch((err)=> {
       console.log(err)
     })
-  }, [])
+  }, [food])
 
-  function displayFlavourCombo(foodObj){
-  const foodId = String(foodObj.id)
-  getAllFlavourCombos(foodId)
-  .then((flavourCombos) => {
-    console.log('flavs', flavourCombos)
-  })
-  .catch((err)=> {
-    console.log(err)
-  })
-  }
-    //NEXT
-    //write function to call apiClient with foodObj.id
-    //map through the results 
-    //return a list of flavour combos
-  
+
 
   return ( 
   <>
-  <h1>//</h1>
+  <ul>{combos.map(item => <li key={item.FlavourId}>{item.flavour_combo}</li>)}</ul>
   </>
    );
 }
