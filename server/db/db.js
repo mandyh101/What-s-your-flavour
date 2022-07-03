159c3b9e6b92069789386af55f14334e751211b4
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-03 16:44:46 +1200

    remove pseudocode

--- server/db/db.js
+++ server/db/db.js
@@ -22,9 +22,3 @@ function getAllFlavoursByFood(foodId, db = connection) {
     .select('food.name', 'food.id', 'flavours.id as flavourId', 'flavour_combo')
     .where('food.id', foodId)
 }
-
-//2. Write getAllFood function
-//3. Wite a function that gets all flavours that match a food by id/food_id
-//4. write a function that gets a flavour combo by ids?
-//3. Write a function to get all flavours that match an id
-//4. Write a function to get all flavours that match an id and select a random one (this one might go inside the routes function?)
