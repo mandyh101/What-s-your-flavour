4b2842e56d0a28abeed9faf7b4ad8d9aaba9d2a7
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-03 16:43:46 +1200

    test getAllFlavoursByFood

--- server/db/db.test.js
+++ server/db/db.test.js
@@ -23,3 +23,14 @@ describe('getAllFood', () => {
     })
   })
 })
+
+describe('getAllFlavoursByFood', () => {
+  test('returns an array of flavours that match a selected food', () => {
+    expect.assertions(2)
+    const foodId = 107
+    return db.getAllFlavoursByFood(foodId, testDb).then((flavours) => {
+      expect(flavours).toHaveLength(3)
+      expect(flavours[2]).toHaveProperty('flavour_combo')
+    })
+  })
+})
