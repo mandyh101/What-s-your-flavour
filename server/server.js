6e4d540f0ef84686b736d19ec03b592c73c2aedb
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-07 19:10:44 +1200

    delete all api stuff as it was in aanother branch

--- server/server.js
+++ server/server.js
@@ -1,16 +1,12 @@
 const path = require('path')
 const express = require('express')
 
-const food = require('./routes/food') 
-
-//external API function
-const recipes = require('./routes/recipes')
+const food = require('./routes/food')
 
 const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/food', food) //internal api
-server.use('/api/v2/recipes', recipes) //external API recipes - might not need this
 
 module.exports = server
