2795719a4511d97b0c00c93ca10274a5bf6fa184
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-12 18:00:19 +1200

    add post route to get recipes by query varible

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -23,4 +23,24 @@ router.get('/', (req, res) => {
   }
 })
 
+//POST '/api/v2/recipes
+router.post('/', (req, res) => {
+  console.log('req.body', req.body)
+  const query = req.body.food //HELP
+  console.log('query', query)
+  try {
+    return request
+      .get(
+        //how do i insert the search query?!
+        `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${apiId}&app_key=${apiKey}`
+      )
+      .then((response) => {
+        console.log('recipe route hit')
+        res.json(response.body.hits)
+      })
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
 module.exports = router
